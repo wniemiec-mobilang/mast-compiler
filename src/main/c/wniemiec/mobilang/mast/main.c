a8cae4158caa74e61ff2a050a3827589a0a696e0
William Niemiec <williamniemiec@hotmail.com>
2022-02-16 22:20:20 -0300

    Git ignore - c files - fix

---
+++ src/main/c/wniemiec/mobilang/mast/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+//-----------------------------------------------------------------------------
+//		Prototypes
+//-----------------------------------------------------------------------------
+extern int yyparse(void);
+extern int yylex_destroy(void);
+
+void *tree = NULL;
+void export_tree(void *tree);
+void free_tree(void *tree);
+
+
+//-----------------------------------------------------------------------------
+//		Main
+//-----------------------------------------------------------------------------
+int main(int argc, char **argv)
+{
+    int ret = yyparse();
+
+    export_tree(tree);
+    free_tree(tree);
+    tree = NULL;
+    yylex_destroy();
+
+    return ret;
+}
